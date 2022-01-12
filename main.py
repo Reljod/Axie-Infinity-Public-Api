dc81976082f18a04489191e8b077a106da873014
Reljod Oreta <oretareljod@gmail.com>
2022-01-13 02:54:00 +0800

    Change API Doc info to official info

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 from fastapi import FastAPI, responses, status
-
+from fastapi.openapi.utils import get_openapi
+from typing import Dict
 from api.ds.datasource_json import CardsSourceFromJSON
 from api.models.cards import CardList, CardFilterBasic
 from api.models.limit import PaginationLimiter
-from api.endpoints import cards_controller as cc
+from api.endpoints import cards_controller as cc 
 
 app = FastAPI()
+
+def custom_openapi() -> Dict[str, any]:
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Axie Infinity Public API",
+        version="0.1.0",
+        description="Open Source public API containing Axie Infinity Related Data.",
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": r"https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+app.openapi = custom_openapi
 
 @app.get("/")
 async def root():
